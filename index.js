12eed8c272567ac8a32560f52a6cb7f4677e3d50
strangeal <bajerijustice@gmail.com>
2022-09-19 13:46:07 -0400

    Add two functions

--- index.js
+++ index.js
@@ -6,3 +6,20 @@ const bookList = document.querySelector('#book-list');
 const formGroup = document.querySelector('#form-container');
 let addOn;
 let bookObj = JSON.parse(localStorage.getItem('saveItem')) || [];
+
+// Add book
+function newAdd() {
+  addOn = {
+    title: titleInput.value,
+    author: authorInput.value,
+    id: Math.floor(Math.random()*1000000),
+  }
+  bookObj.push(addOn);
+  localStorage.setItem('saveItem', JSON.stringify(bookObj));
+}
+
+// Remove
+function removeBook(id) {
+  bookObj = bookObj.filter(filterObj => filterObj.id !==id);
+  localStorage.setItem('saveItem', JSON.stringify(bookObj));
+}
