50cb854e1961eae1ee0ac617d38e1152fa34c54b
Isiteketo Mutau <mutau85@gmail.com>
2022-09-21 09:57:18 +0200

    Add book classes functionality

--- index.js
+++ index.js
@@ -5,3 +5,33 @@ class Book {
     this.author = author;
   }
 }
+
+// Add books classas
+class displayOn {
+  static displayBooks() {
+    const books = storageStore.getBooks();
+    books.forEach((bookAddOn) => displayOn.addToBooks(bookAddOn));
+  }
+  static addToBooks(bookAddOn) {
+    const tableDiv = document.createElement('div');
+    tableDiv.classList.add('collection');
+    tableDiv.innerHTML = `
+    <div class="collection-text">
+    <p>"${bookAddOn.title}"</p>
+    <p>by</p>
+    <p>${bookAddOn.author}</p>
+    </div>
+    <button type="submit" class="delete">Remove</button>
+    `;
+    bookList.appendChild(tableDiv);
+  }
+  static deleteBook(el) {
+    if(el.classList.contains('delete')) {
+      el.parentElement.remove();
+    }
+  }
+  static clearInputFields() {
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
+  }
+}
